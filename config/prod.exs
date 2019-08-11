5829aa29664beab203ed70bdfde000486b0877cc
Tina van Schelt <tina@Tinas-MacBook-Pro.local>
2019-08-11 22:50:25 +0100

    update config/prod

--- config/prod.exs
+++ config/prod.exs
@@ -10,6 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :strava_age_grading, StravaAgeGradingWeb.Endpoint,
+  load_from_system_env: true,
   url: [scheme: "https", host: "strava-age-grading", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
