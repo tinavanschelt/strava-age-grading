0e9b5f9ae93d424cbd7aee414ee72dd9d5391aa1
Tina van Schelt <tina@Tinas-MacBook-Pro.local>
2019-08-11 22:44:25 +0100

    update config/prod

--- config/prod.exs
+++ config/prod.exs
@@ -17,6 +17,12 @@ config :strava_age_grading, StravaAgeGradingWeb.Endpoint,
 # Do not print debug messages in production
 config :logger, level: :info
 
+config :strava_age_grading, StravaAgeGradingWeb.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  #ssl: true
+
 # ## SSL Support
 #
 # To get SSL working, you will need to add the `https` key
